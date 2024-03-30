a5cd75a976e5e3e67f0ffadd76c61340f5508ed4
fernando <isaacfernando5351@gmail.com>
2024-03-29 20:24:15 -0600

    fix error

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,7 @@ const userRouter = require('./user.routes.js');
 const authRouter = require('./auth.routes.js');
 
 function routesHandler(app) {
+    //fix bug
     app.use('/api/v1', skipAuthentication, router);
     router.use('/role/permissions', rolePermissionRouter);
     router.use('/role', roleRouter);
