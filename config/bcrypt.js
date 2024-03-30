5985ff132a7146484a77dda5195e73723d021115
fernando <isaacfernando5351@gmail.com>
2024-03-29 20:17:36 -0600

    delete all for errors

--- config/bcrypt.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function encript(password) {
-    const hash = await bcrypt.hash(password, 10);
-    console.log(hash);
-    return hash;
-}
-
-encript('Papaya@123');
--- src/auth/index.js
+++
@@ -1,7 +0,0 @@
-const passport = require('passport');
-const localStrategy = require('./strategies/passport.local');
-const { jwt, recoveryPassword } = require('./strategies/passport.jwt');
-
-passport.use(localStrategy);
-passport.use('jwt', jwt);
-passport.use('recoveryPassword', recoveryPassword);
