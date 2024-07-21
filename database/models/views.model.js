7d7e956badd193307d0d8bc95f10c56fef023614
fernando <isaacfernando5351@gmail.com>
2024-07-21 13:39:23 -0600

    views model

---
+++ database/models/views.model.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const ViewsModel = {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: true
+    },
+    rolView: {
+
+    },
+    userView: {
+
+    },
+    View: {
+
+    },
+    View: {
+
+    },
+}
